6efb25b3483c2140b0487485a9b0ab1fc5e9cc01
Anand <ak1289@gmail.com>
2017-10-28 13:53:30 +0530

    Adding Function

--- string.c
+++ string.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 
-size_t my_strlen(char *s)
+return (p -s );
+}
+char *my_strcpy(char *t, char *s)
+
 {
-   char *p = s;
-
-   while (*p)
-      ++p;
-
-   return (p - s);
+   
+   char *p = t;
+   
+    while (*t = *s)
+    ;
+   
+   
+   return p;
+   
 }
 
 int main(void)
 {
-   int i;
+   int i; 
+   
+   char p1[32];
    char *s[] = 
    {
       "Git tutorials",
       "Tutorials Point"
-   };
-
-   for (i = 0; i < 2; ++i)
+      @@ -20,5 31,7 @@ int main(void)
+      for (i = 0; i < 2; i)
+      printf("string lenght of %s = %lu\n", s[i], my_strlen(s[i]));
       
-   printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
-
-   return 0;
+      printf("%s\n", my_strcpy(p1, "Hello, World !!!"));
+      
+      return 0;
+   }
 }
