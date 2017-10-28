791c51005719492ddec5949af62f0f297e15d178
Anand <ak1289@gmail.com>
2017-10-28 13:44:35 +0530

    Revert Jerry Changes

--- string.c
+++ string.c
@@ -1,34 +1,27 @@
-return (p -s );
-}
-+char *my_strcpy(char *t, char *s)
-+
+#include <stdio.h>
+
+size_t my_strlen(const char *s)
 {
-   +
-   char *p = t;
-   +
-   + while (*t++ = *s++)
-   + ;
-   +
-   +
-   return p;
-   +
+   char *p = s;
+
+   while (*p)
+      ++p;
+
+   return (p - s);
 }
-+
+
 int main(void)
 {
-   int i; 
-   +
-   char p1[32];
+   int i;
    char *s[] = 
    {
       "Git tutorials",
       "Tutorials Point"
-      @@ -20,5 +31,7 @@ int main(void)
-      for (i = 0; i < 2; ++i)
-      printf("string lenght of %s = %lu\n", s[i], my_strlen(s[i]));
-      +
-      printf("%s\n", my_strcpy(p1, "Hello, World !!!"));
-      +
-      return 0;
-   }
+   };
+
+   for (i = 0; i < 2; ++i)
+      
+   printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+
+   return 0;
 }
