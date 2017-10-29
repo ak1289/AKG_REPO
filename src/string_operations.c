d8abffc3e6cf764596db2d47bb5e57e058848be9
Anand <ak1289@gmail.com>
2017-10-29 14:57:15 +0530

    Resolve conflict

--- src/string_operations.c
+++ src/string_operations.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <wchar.h>
-<<<<<<< HEAD
 size_t My_WC_strlen(const wchar_t *s, wchar_t *t)
-=======
-size_t My_WC_strlen(const wchar_t *s, wchar_t *wc)
->>>>>>> 8b68f2100fb315652ee22aa7cf37432c16a5ad7c
 {
    const wchar_t *p = s;
 
